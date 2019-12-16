210605519095ff0425359d7272ccb78c773ef345
Dybbor <19dobrohotov99@mail.ru>
2019-12-16 17:18:49 +0300

    lab4 done

--- samples/Wavelet.cpp
+++ samples/Wavelet.cpp
@@ -6,6 +6,47 @@
 using namespace cv;
 using namespace std;
 
+int HowMuchPorog(double** data, int rows, int cols,double porog) 
+{
+	int count = 0;
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			if (abs(data[i][j]) < porog)
+				count++;
+	return count;
+}
+void DeleteLessPorog(double**data, int rows, int cols, double porog) 
+{
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			if (abs(data[i][j]) < porog)
+				data[i][j] = 0;
+}
+
+int Diff(Mat image1, Mat image2) 
+{
+	int count = 0;
+	for (int i=0;i<image1.rows;i++)
+		for (int j = 0; j < image1.cols;j++) 
+			if (image1.at<uchar>(i, j) != image2.at<uchar>(i, j))
+				count++;
+	return count;
+}
+
+
+void showDiff(Mat orig, Mat diff)
+{
+	namedWindow("Diff", WINDOW_NORMAL);
+	for (int i = 0; i < orig.rows; i++)
+		for (int j = 0; j < orig.cols; j++)
+		{
+			if (abs(orig.at<uchar>(i, j) - diff.at<uchar>(i, j)) > 10)
+				diff.at<uchar>(i, j) = 0;
+		}
+	imshow("Diff", orig);
+	cout << " awdlajwdk " << Diff(orig, diff) << endl;;
+}
+
 void PrintData(double** data, int rows, int cols) 
 {
 	cout << endl;
@@ -18,7 +59,7 @@ void PrintData(double** data, int rows, int cols)
 		cout << endl;
 	}
 }
-void WaveletRows(double** data, int rows, int cols)
+void WaveletCols(double** data, int rows, int cols)
 {
 	for (int i = 0; i < rows; i++)
 	{
@@ -32,7 +73,21 @@ void WaveletRows(double** data, int rows, int cols)
 		}
 	}
 }
-void WaveletCols(double** data, int rows, int cols)
+void BackWaveletCols(double**data,int rows, int cols) 
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j+=2) 
+		{
+			double tmp1, tmp2;
+			tmp1 = data[i][j] + data[i][j + 1];
+			tmp2= data[i][j] - data[i][j + 1];
+			data[i][j] = tmp1;
+			data[i][j + 1] = tmp2;
+		}
+	}
+}
+void WaveletRows(double** data, int rows, int cols)
 {
 	for (int j = 0; j < cols; j++)
 	{
@@ -46,38 +101,107 @@ void WaveletCols(double** data, int rows, int cols)
 		}
 	}
 }
-double** SeparateRows(double** data, int rows, int cols)
+void BackWaveletRows(double**data, int rows, int cols)
+{
+	for (int j = 0; j < cols; j++)
+	{
+		for (int i = 0; i < rows; i += 2)
+		{
+			double tmp1, tmp2;
+			tmp1 = data[i][j] + data[i+1][j];
+			tmp2 = data[i][j] - data[i+1][j];
+			data[i][j] = tmp1;
+			data[i+1][j] = tmp2;
+		}
+	}
+}
+void SeparateCols(double** data, int rows, int cols, int coef)
 {
 	double** tmp_data = new double*[rows];
 	for (int i = 0; i < rows; i++)
 		tmp_data[i] = new double[cols];
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			tmp_data[i][j] = data[i][j];
 	int move;
-	for (int i = 0; i < rows; i++) {
+	for (int i = 0; i < rows/coef; i++) {
 		move = 0;
-		for (int j = 0; j < cols; j += 2) {
-			tmp_data[i][move] = data[i][j];
-			tmp_data[i][cols / 2 + move] = data[i][j + 1];
+		for (int j = 0; j < cols/coef; j += 2) {
+			data[i][move] = tmp_data[i][j];
+			data[i][cols / 2/ coef + move] = tmp_data[i][j + 1];
 			move++;
 		}
 	}
-	return tmp_data;
+	for (int i = 0; i < rows; i++)
+		delete[] tmp_data[i];
+	delete[] tmp_data;
 }
-double** SeparateCols(double** data, int rows, int cols)
+void BackSeparateCols(double** data, int rows, int cols, int coef)
 {
 	double** tmp_data = new double*[rows];
 	for (int i = 0; i < rows; i++)
 		tmp_data[i] = new double[cols];
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			tmp_data[i][j] = data[i][j];
 	int move;
-	for (int j = 0; j < cols; j++) {
+	for (int i = 0; i < rows / coef; i++) {
 		move = 0;
-		for (int i = 0; i < rows;  i+= 2) {
-			tmp_data[move][j] = data[i][j];
-			tmp_data[rows/2+move][j] = data[i+1][j];
+		for (int j = 0; j < cols / coef; j += 2) {
+			data[i][j] = tmp_data[i][move];
+			data[i][j+1] = tmp_data[i][cols / 2 / coef + move];
 			move++;
 		}
 	}
-	return tmp_data;
+	for (int i = 0; i < rows; i++)
+		delete[] tmp_data[i];
+	delete[] tmp_data;
 }
+
+void SeparateRows(double** data, int rows, int cols,int coef)
+{
+	double** tmp_data = new double*[rows];
+	for (int i = 0; i < rows; i++)
+		tmp_data[i] = new double[cols];
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			tmp_data[i][j] = data[i][j];
+	int move;
+	for (int j = 0; j < cols/coef; j++) {
+		move = 0;
+		for (int i = 0; i < rows/coef;  i+= 2) {
+			data[move][j] = tmp_data[i][j];
+			data[rows/2/coef+move][j] = tmp_data[i+1][j];
+			move++;
+		}
+	}
+	for (int i = 0; i < rows; i++)
+		delete[] tmp_data[i];
+	delete[] tmp_data;
+}
+
+void BackSeparateRows(double** data, int rows, int cols, int coef)
+{
+	double** tmp_data = new double*[rows];
+	for (int i = 0; i < rows; i++)
+		tmp_data[i] = new double[cols];
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < cols; j++)
+			tmp_data[i][j] = data[i][j];
+	int move;
+	for (int j = 0; j < cols / coef; j++) {
+		move = 0;
+		for (int i = 0; i < rows / coef; i += 2) {
+			data[i][j] = tmp_data[move][j];
+			data[i+1][j] = tmp_data[rows / 2 / coef + move][j];
+			move++;
+		}
+	}
+	for (int i = 0; i < rows; i++)
+		delete[] tmp_data[i];
+	delete[] tmp_data;
+}
+
 Mat DataToMat(double** data, int rows, int cols) 
 {
 	Mat image(rows, cols, CV_8UC1);
@@ -89,13 +213,22 @@ Mat DataToMat(double** data, int rows, int cols)
 int main() 
 {
 	Mat image = imread("D:\\GitProject\\ImageProcessing\\picture\\ship.jpg");
+	Mat wl1, wl2, wl3;
+	Mat dl1, dl2, dl3;
+	double porog = 5;
+	cout << "porog " << porog << endl;
 	if (!image.data) 
 	{
 		cout << "Error immage load" << endl;
 		return -1;
 	}
-	namedWindow("image", WINDOW_NORMAL);
-	namedWindow("wl1",WINDOW_NORMAL); //wavelet level 1
+	//namedWindow("image", WINDOW_NORMAL);
+	//namedWindow("wl1",WINDOW_NORMAL); //wavelet level 1
+	//namedWindow("wl2",WINDOW_NORMAL); //wavelet level 2
+	//namedWindow("wl3",WINDOW_NORMAL); //wavelet level 3
+	//namedWindow("dl1", WINDOW_NORMAL); //decoder level 1
+	//namedWindow("dl2", WINDOW_NORMAL); //decoder level 2
+	//namedWindow("dl3", WINDOW_NORMAL); //decoder level 3
 	if (image.channels() >1 )
 		cvtColor(image,image, COLOR_BGR2GRAY);
 	double**data = new double*[image.rows];
@@ -103,21 +236,60 @@ int main()
 	{
 		data[i] = new double[image.cols];
 	}
-	for (int i = 0; i < image.rows; i++) {
+	for (int i = 0; i < image.rows; i++)
 		for (int j = 0; j < image.cols; j++)
-		{
 			data[i][j] = (int)image.at<uchar>(i, j);
-			//cout << (int)image.at<uchar>(i, j) << " ";
-		}
-		//cout << endl;
-	}
+	//level 1
 	WaveletRows(data, image.rows, image.cols);
-	data = SeparateRows(data, image.rows, image.cols);
+	SeparateRows(data, image.rows, image.cols,1);
 	WaveletCols(data, image.rows, image.cols);
-	data = SeparateCols(data, image.rows, image.cols);
-	//PrintData(data, image.rows, image.cols);
+	SeparateCols(data, image.rows, image.cols,1);
+	wl1 = DataToMat(data, image.rows, image.cols);
+	//level 2
+	WaveletRows(data, image.rows/2, image.cols/2);
+	SeparateRows(data, image.rows, image.cols,2);
+	WaveletCols(data, image.rows/2, image.cols/2);
+	SeparateCols(data, image.rows, image.cols,2);
+	wl2 = DataToMat(data, image.rows, image.cols);
+	//level 3
+	WaveletRows(data, image.rows / 4, image.cols / 4);
+	SeparateRows(data, image.rows, image.cols, 4);
+	WaveletCols(data, image.rows / 4, image.cols / 4);
+	SeparateCols(data, image.rows, image.cols, 4);
+	wl3 = DataToMat(data, image.rows, image.cols);
+	imwrite("D:\\GitProject\\ImageProcessing\\picture\\save_without_porog.jpg", wl3);
+	cout << "count porog " << HowMuchPorog(data, image.rows, image.cols, porog) << endl;
+	DeleteLessPorog(data, image.rows, image.cols, porog);
+	//decoder level 3
+	BackSeparateCols(data, image.rows, image.cols, 4);
+	BackWaveletCols(data, image.rows / 4, image.cols / 4);
+	BackSeparateRows(data, image.rows, image.cols, 4);
+	BackWaveletRows(data, image.rows / 4, image.cols / 4);
+	dl3 = DataToMat(data, image.rows, image.cols);
+	//decoder level 2
+	BackSeparateCols(data, image.rows, image.cols,2);
+	BackWaveletCols(data, image.rows/2, image.cols/2);
+	BackSeparateRows(data, image.rows, image.cols,2);
+	BackWaveletRows(data, image.rows/2, image.cols/2);
+	dl2= DataToMat(data, image.rows, image.cols);
+	//decoder level 1
+	BackSeparateCols(data, image.rows, image.cols, 1);
+	BackWaveletCols(data, image.rows , image.cols);
+	BackSeparateRows(data, image.rows, image.cols,1);
+	BackWaveletRows(data, image.rows , image.cols);
+	dl1 = DataToMat(data, image.rows, image.cols);
+	//showDiff(image, dl1);
+	//threshold(wl3, wl3, 15, 255, THRESH_BINARY);
+	imwrite("D:\\GitProject\\ImageProcessing\\picture\\save_with_porog.jpg", wl3);
+	imwrite("D:\\GitProject\\ImageProcessing\\picture\\grey.jpg", image);
 	imshow("image", image);
-	imshow("wl1", DataToMat(data, image.rows, image.cols));
+	imshow("wl1", wl1);
+	imshow("wl2", wl2);
+	imshow("wl3", wl3);
+	imshow("dl3", dl3);
+	imshow("dl2", dl2);
+	imshow("dl1", dl1);
+	cout << "loss: " <<(double) ( (double)(Diff(image, dl1)*100)) / (image.rows*image.cols) << endl;
 	waitKey();
 	for (int i = 0; i < image.rows; i++)
 		delete[] data[i];
@@ -125,271 +297,3 @@ int main()
 	return 0;
 }
 
-//#include <opencv2/highgui.hpp>
-//#include <opencv2/opencv.hpp>
-//#include <math.h>
-//#include <vector>
-//using namespace cv;
-//using namespace std;
-//
-//auto calculateLowCoef() 
-//{
-//	vector<double> lowCoef;
-//	lowCoef.push_back((1 + sqrt(3)) / (4 * sqrt(2)));
-//	lowCoef.push_back((3 + sqrt(3)) / (4 * sqrt(2)));
-//	lowCoef.push_back((3 - sqrt(3)) / (4 * sqrt(2)));
-//	lowCoef.push_back((1 - sqrt(3)) / (4 * sqrt(2)));
-//	return lowCoef;
-//}
-//auto calculateHighCoef(vector<double> lowCoef) 
-//{
-//	vector<double> highCoef(lowCoef.size());
-//	for (int i = 0; i < lowCoef.size(); i++) 
-//	{
-//		highCoef[i]= pow(-1, i)*lowCoef[lowCoef.size() - i - 1];
-//	}
-//	return highCoef;
-//}
-//auto pairConvolution(vector<double>data, vector<double> lowCoef, vector <double> highCoef,int delta=0) 
-//{
-//	int n = lowCoef.size();
-//	int m = data.size();
-//	vector <double> res;
-//	for (int i = 0; i < m; i += 2) 
-//	{
-//		double sL = 0;
-//		double sH = 0;
-//		for (int j = 0; j < n; j++) 
-//		{
-//			sL += data[(i + j - delta) % m] * lowCoef[j];
-//			sH += data[(i + j - delta) % m] * highCoef[j];
-//		}
-//		res.push_back(sL);
-//		res.push_back(sH);
-//	}
-//	/*for (auto it = res.begin(); it != res.end(); it++)
-//		cout << *it << endl;*/
-//	return res;
-//}
-//auto backCoeff(vector <double> lowCoef, vector <double> highCoef)
-//{
-//	vector <double> res;
-//	vector <double> tmp;
-//	for (int i = 0; i < lowCoef.size(); i += 2)
-//	{
-//		int pos = i - 2;
-//		if (i - 2 < 0)
-//			pos = lowCoef[lowCoef.size() - 2];
-//		res.push_back(lowCoef[pos]);
-//		res.push_back(highCoef[pos]);
-//		tmp.push_back(lowCoef[pos+1]);
-//		tmp.push_back(highCoef[pos+1]);
-//	}
-//	for (int i = 0; i < highCoef.size(); i++)
-//		res.push_back(tmp[i]);
-//	return res;
-//}
-//auto getRow(Mat image, int n) 
-//{
-//	vector <double>res;
-//		for (int i = 0; i < image.cols; i++)
-//		{
-//			res.push_back(image.at<uchar>(n, i));
-//		}
-//		
-//		return res;
-//}
-//
-//auto getCol(Mat image, int n)
-//{
-//	vector <double> res;
-//	for (int i = 0; i < image.cols; i++)
-//	{
-//		res.push_back(image.at<uchar>(i,n));
-//	}
-//	
-//	return res;
-//}
-//auto setRow(Mat image, int n, vector <double> row ) 
-//{
-//	for (int i = 0; i < image.cols; i++)
-//		image.at <uchar>(n, i) = (int)row[i];
-//	return image;
-//}
-//auto setCol(Mat image, int n, vector <double> col)
-//{
-//	for (int i = 0; i < image.rows; i++)
-//		image.at <uchar>(i, n) = (int)col[i];
-//	return image;
-//}
-//Mat Wavelet(Mat image, vector<double> lowCoef) 
-//{
-//	vector <double> highCoef = calculateHighCoef(lowCoef);
-//	Mat copy = image.clone();
-//	for (int i = 0; i < image.rows; i++)
-//	{
-//		setRow(copy, i, pairConvolution(getRow(image, i), lowCoef, highCoef));
-//	}
-//	for (int i = 0; i < image.cols; i++) 
-//	{
-//		setCol(copy, i, pairConvolution(getCol(image, i), lowCoef, highCoef));
-//	}
-//	namedWindow("Copy", WINDOW_NORMAL);
-//	imshow("Copy", copy);
-//	return copy;
-//	
-//}
-//Mat Transposition(Mat image) 
-//{
-//	Mat copy = image.clone();
-//	int k = 0;
-//	int l = 0;
-//	for (int i = 0; i < image.rows / 2; i++)
-//	{
-//		 l = 0;
-//		for (int j = 0; j < image.cols / 2; j++)
-//		{
-//			copy.at<uchar>(i, j) = image.at<uchar>(k, l);
-//			l += 2;
-//		}
-//		k += 2;
-//	}
-//	k = 1;
-//	for (int i = image.rows/2; i < image.rows; i++)
-//	{
-//		 l = 0;
-//		for (int j = 0; j < image.cols / 2; j++)
-//		{
-//			copy.at<uchar>(i, j) = image.at<uchar>(k, l);
-//			l += 2;
-//		}
-//		k += 2;
-//	}
-//	k = 0;
-//	for (int i = 02; i < image.rows/2; i++)
-//	{
-//		 l = 1;
-//		for (int j = image.cols/2; j < image.cols ; j++)
-//		{
-//			copy.at<uchar>(i, j) = image.at<uchar>(k, l);
-//			l += 2;
-//		}
-//		k += 2;
-//	}
-//	k = 1;
-//	for (int i = image.rows/2; i < image.rows; i++)
-//	{
-//		 l = 1;
-//		for (int j = image.cols/2; j < image.cols; j++)
-//		{
-//			copy.at<uchar>(i, j) = image.at<uchar>(k, l);
-//			l += 2;
-//		}
-//		k += 2;
-//	}
-//	namedWindow("Transposition", WINDOW_NORMAL);
-//	imshow("Transposition", copy);
-//	return copy;
-//}
-//// тут ффигня надо переделовать
-////Mat backTransposition(Mat image)
-////{
-////	int k = 0, l = 0;
-////	Mat copy = image.clone();
-////	for (int i = 0; i < image.rows / 2; i++)
-////	{
-////		for (int j = 0; j < image.cols / 2; j++)
-////		{
-////			
-////		}
-////	}
-////	namedWindow("Back Transposition", WINDOW_NORMAL);
-////	imshow("Back Transposition", copy);
-////	waitKey();
-////	for (int i = image.rows / 2; i < image.rows; i++)
-////	{
-////		int k = 1, l = 0;
-////		for (int j = 0; j < image.cols / 2; j++)
-////		{
-////			copy.at<uchar>(k, l) = image.at<uchar>(i, j);
-////			k += 2;
-////			l += 2;
-////		}
-////	}
-////	for (int i = 02; i < image.rows / 2; i++)
-////	{
-////		int k = 0, l = 1;
-////		for (int j = image.cols / 2; j < image.cols; j++)
-////		{
-////			copy.at<uchar>(k, l) = image.at<uchar>(i, j);
-////			k += 2;
-////			l += 2;
-////		}
-////	}
-////	for (int i = image.rows / 2; i < image.rows; i++)
-////	{
-////		int k = 1, l = 1;
-////		for (int j = image.cols / 2; j < image.cols; j++)
-////		{
-////			copy.at<uchar>(k, l) = image.at<uchar>(i, j);
-////			k += 2;
-////			l += 2;
-////		}
-////	}
-////	namedWindow("Back Transposition", WINDOW_NORMAL);
-////	imshow("Back Transposition", copy);
-////	return copy;
-////}
-//Mat hist_intensity(Mat img) {
-//	int gist_intensity[256];
-//	for (int i = 0; i < 256; i++)
-//		gist_intensity[i] = 0;
-//	int Max_value = 0;;
-//	for (int i = 0; i < img.rows; i++) {
-//		for (int j = 0; j < img.cols; j++) {
-//			gist_intensity[img.at<uchar>(i, j)]++;
-//		}
-//	}
-//	for (int i = 0; i < 256; i++)
-//		if (gist_intensity[i] > Max_value)Max_value = gist_intensity[i];
-//	while (Max_value > 700) {
-//		for (int i = 0; i < 256; i++) {
-//			gist_intensity[i] /= 2;
-//		}
-//		Max_value /= 2;
-//	}
-//	Point p(0, Max_value);
-//	Mat result(Max_value, 1290, CV_8UC1);
-//	cout << Max_value << endl << result.rows << endl;
-//	for (int i = 0; i < 256; i++) {
-//		if (gist_intensity[i] > 1)
-//		{
-//			line(result, p, Point(p.x, result.rows - gist_intensity[i]), Scalar(255, 255, 255), 2);
-//			p.x += 5;
-//		}
-//	}
-//	return result;
-//}
-//
-//int main() 
-//{
-//	Mat image = imread("D:\\GitProject\\OU\\picture\\ship.jpg");
-//	if (!image.data) 
-//	{
-//		cout << "Error immage load" << endl;
-//		return -1;
-//	}
-//	if (image.channels() >1 )
-//
-//		cvtColor(image,image, COLOR_BGR2GRAY);
-//	vector<double> lowCoef = calculateLowCoef();
-//	vector<double> highCoef= calculateHighCoef(lowCoef);
-//	namedWindow("original", WINDOW_NORMAL);
-//	imshow("original", image);
-//	Transposition(Wavelet(image, lowCoef));
-//	//imwrite("D:/GitProject/OU/picture/image.jpg",Transposition(Wavelet(image, lowCoef)));
-//	
-//	waitKey();
-//
-//	return 0;
-//}
